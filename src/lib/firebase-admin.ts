ed68c052d3643df95c27fdc554340251330348a2
Evod lc <evod599@gmail.com>
2025-07-31 15:14:19 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,16 +1,10 @@
 import admin from 'firebase-admin';
-
-// This is a more robust way to initialize, especially for serverless environments
-// where process.env might not be populated as expected.
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-};
+import serviceAccount from './serviceAccountKey';
 
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
+      // @ts-ignore
       credential: admin.credential.cert(serviceAccount),
       databaseURL: `https://testpoint-81a9a.firebaseio.com`,
     });
