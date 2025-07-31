7808458a850016edbad593308e4b91044c658ffd
Evod lc <evod599@gmail.com>
2025-07-31 15:28:37 +0000

    i have updated the .env.local file donot change it but use it in code

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,12 +1,25 @@
 import admin from 'firebase-admin';
-import serviceAccount from './serviceAccountKey';
 
+// Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
   try {
+    if (
+      !process.env.FIREBASE_PROJECT_ID ||
+      !process.env.FIREBASE_CLIENT_EMAIL ||
+      !process.env.FIREBASE_PRIVATE_KEY
+    ) {
+      throw new Error('Firebase environment variables are not set.');
+    }
+
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+
     admin.initializeApp({
-      // @ts-ignore
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: `https://testpoint-81a9a.firebaseio.com`,
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: privateKey,
+      }),
+      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`,
     });
   } catch (error: any) {
     console.error('Firebase admin initialization error', error.stack);
